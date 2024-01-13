ccbdef6da91adb845fde49849d0062ad4c60012f
Anderson Menezes <andersonmenezes.dev@gmail.com>
2024-01-12 22:04:42 -0300

    initial commit with base deps

---
+++ src/main/java/com/andersonHSM/springshop/SpringShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.andersonHSM.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersonHSM/springshop/SpringShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonHSM.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
