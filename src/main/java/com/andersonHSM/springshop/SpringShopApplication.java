153a50dee1d5f2bb0bd6155f953ab447f7d36cb4
Anderson Menezes <andersonmenezes.dev@gmail.com>
2024-01-17 11:48:01 -0300

    adding kafka support on spring base-app

--- src/main/java/com/andersonHSM/springshop/SpringShopApplication.java
+++ src/main/java/com/andersonHSM/springshop/SpringShopApplication.java
@@ -1,13 +1,51 @@
 package com.andersonHSM.springshop;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopApplication.class, args);
+	}
+
+	@Bean
+	public KafkaAdmin admin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092");
+		return new KafkaAdmin(configs);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(id = "myId", topics = "topic1")
+	public void listen(String in) {
+		System.out.println(in);
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			template.send("topic1", "my-id", "test message");
+		};
 	}
 
 }
